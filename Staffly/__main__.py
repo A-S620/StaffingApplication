7dee16025f4dce02d529c59c3ec90513b9ff7cb3
K-itKat20 <LaurelKatie@tutanota.com>
2020-06-21 20:34:22 +0100

    Days off table created and connected to days off page.

--- Staffly/__main__.py
+++ Staffly/__main__.py
@@ -5,10 +5,10 @@ __package__ == 'Staffly'
 # Imports from project
 # from Staffly.UI.navigation import navigation
 from Staffly import navigation
-
-
-
+from Staffly.controller import Database
 
 if __name__ == '__main__':
     print("looooool")
+    #Database.createDaysOff()
+
     navigation.createUI()
--- Staffly/daysOff.py
+++ Staffly/daysOff.py
@@ -1,30 +1,49 @@
-import os
-from kivy.lang import Builder
-from kivy.factory import Factory
-from kivy.properties import ObjectProperty, StringProperty
-from kivymd.app import MDApp
+from datetime import date
 from kivymd.uix.menu import MDDropdownMenu
 from kivymd.uix.picker import MDDatePicker
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.screenmanager import Screen
 from kivy.core.window import Window
-from kivy.uix.spinner import Spinner
-from kivymd.uix.list import IRightBodyTouch, OneLineAvatarIconListItem
-from kivymd.uix.selectioncontrol import MDCheckbox
-from kivymd.icon_definitions import md_icons
+from Staffly.model import DaysOffRequest
+from Staffly.controller.Database import createDaysOffRequest
+
+request_date = date.today()
+request = DaysOffRequest.DaysOffRequest(0, request_date, request_date, request_date, "test")
 class daysOff(Screen):
     menu: MDDropdownMenu
     window_sizes = Window.size
     print("window" + str(window_sizes))
-    def holidayType(self):
-        print (1)
-    def sickType(self):
-        print (2)
 
+    def vacationType(self):
+        request.setTypeId(1)
+        print(request.type_id)
 
+    def desHolidayType(self):
+        request.setTypeId(2)
+        print(request.type_id)
+
+    def illnessType(self):
+        request.setTypeId(3)
+        print(request.type_id)
+
+    def nonTrainType(self):
+        request.setTypeId(4)
+        print(request.type_id)
+
+    def workTrainType(self):
+        request.setTypeId(5)
+        print(request.type_id)
+
+    def militaryType(self):
+        request.setTypeId(6)
+        print(request.type_id)
+
+    def parentType(self):
+        request.setTypeId(7)
+        print(request.type_id)
 
     def getComment(self):
         comment = self.ids.comment.text
+        request.setComment(comment)
         print(comment)
 
     def setStartDate(self):
@@ -38,10 +57,19 @@ class daysOff(Screen):
     def startDate(self, the_date):
         print(the_date)
         self.ids.start_date_label.text = str(the_date)
+        request.setStartDate(the_date)
 
     def endDate(self, the_date):
         print(the_date)
         self.ids.end_date_label.text = str(the_date)
+        request.setEndDate(the_date)
+
+    def createDaysOff(self):
+        print(request.type_id, request.request_date, request.start_date, request.end_date, request.comment)
+        createDaysOffRequest(request.type_id, request.request_date, request.start_date, request.end_date,
+                             request.comment)
+
+        print("test")
 
 # if __name__ == "__main__":
 # swap().run()
